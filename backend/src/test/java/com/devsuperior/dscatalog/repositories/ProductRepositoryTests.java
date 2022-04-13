5b0bfaf68c8a7f1dfb39369b65dc929a1db737da
westorres9 <wester.torres9@gmail.com>
2022-04-12 22:31:27 -0300

    integration test Repository delete when id Not Exists

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -24,4 +25,14 @@ public class ProductRepositoryTests {
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			long nonExistingId = 1000L;
+			repository.deleteById(nonExistingId);
+		});
+	}
+	
 }

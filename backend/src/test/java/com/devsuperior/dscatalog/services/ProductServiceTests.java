30953764a226716877b1d18a0c9e1a629cb5d5d2
westorres9 <wester.torres9@gmail.com>
2022-04-12 23:28:09 -0300

    discussion of use cases for @Mock and @MockBean

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -2,7 +2,11 @@ package com.devsuperior.dscatalog.services;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -10,5 +14,10 @@ public class ProductServiceTests {
 	@InjectMocks
 	private ProductService service;
 	
+	@Mock //usada quando classe de teste não carrega contexto da aplicação, para testes mais isolados
+	private ProductRepository repository; // normalmente usada junto com @ExtendWith  +rapido + enxuto
+	
+	@MockBean //usada quando classes de teste carrega contexto da aplicação, e precisa carregar algum bean do sistema
+	private ProductRepository repository2;//@WebMvcTest    @SpringBootTest   +lento
 	
 }

b8dd8c5e48beca4db03826fe3e625731d2ffbc0e
westorres9 <wester.torres9@gmail.com>
2022-04-12 23:20:16 -0300

    First steps to UnitTests in ProductService using ExtendWith(SpringExtension.class) and @InjectMocks

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	
+}

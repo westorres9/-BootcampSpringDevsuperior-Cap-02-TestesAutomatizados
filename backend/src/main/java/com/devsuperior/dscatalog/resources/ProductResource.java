739a4581ae936cc389816946f884a25ffe8057bd
westorres9 <wester.torres9@gmail.com>
2022-04-13 10:11:24 -0300

    Test in ProductResource FindAll should return page

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -1,17 +1,23 @@
 package com.devsuperior.dscatalog.resources;
 
-import com.devsuperior.dscatalog.DTO.CategoryDTO;
-import com.devsuperior.dscatalog.DTO.ProductDTO;
-import com.devsuperior.dscatalog.services.CategoryService;
-import com.devsuperior.dscatalog.services.ProductService;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import com.devsuperior.dscatalog.DTO.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -1,11 +1,23 @@
 package com.devsuperior.dscatalog.resources;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.devsuperior.dscatalog.DTO.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
@@ -15,4 +27,24 @@ public class ProductResourceTests {
 	
 	@MockBean
 	private ProductService service;
+	
+	private PageImpl<ProductDTO> page;
+	private ProductDTO productDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		productDTO = Factory.createProductDTO();
+		
+		page = new PageImpl<>(List.of(productDTO));
+		
+		when(service.findAll(ArgumentMatchers.any())).thenReturn(page);
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() throws Exception {
+		mockMvc.perform(get("/products")).andExpect(status().isOk());
+	}
+	
+	
 }

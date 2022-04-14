eae4921bf5b9a9fff15bf0c03ada359259fc34d7
westorres9 <wester.torres9@gmail.com>
2022-04-14 00:43:32 -0300

    Integration Tests in ProductService Layer with methods findAll  when Return Page Non empty, when return Page empty and when Page sorted by Name

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -1,13 +1,14 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.DTO.ProductDTO;
 import com.devsuperior.dscatalog.entities.Category;
@@ -17,22 +18,24 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.Factory;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long existingId;
 	private long nonExistingId;
 	private Long countTotalProducts;
-	private PageImpl<Product> page;
+	private PageRequest page;
+	private PageRequest emptyPage;
 	private Product product;
 	private ProductDTO productDTO;
 	private Category category;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
@@ -40,22 +43,60 @@ public class ProductServiceIT {
 		countTotalProducts = 25L;
 		product = Factory.createProduct();
 		productDTO = Factory.createProductDTO();
-		page = new PageImpl<>(List.of(product));
+
 		category = Factory.createCategory();
 	}
+
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+
+		page = PageRequest.of(0, 10);
+
+		Page<ProductDTO> result = service.findAll(page);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExists() {
+
+		emptyPage = PageRequest.of(50, 10);
+
+		Page<ProductDTO> result = service.findAll(emptyPage);
+
+		Assertions.assertTrue(result.isEmpty());
+	}
+
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortedByName() {
+
+		page = PageRequest.of(0, 10, Sort.by("name"));
+
+		Page<ProductDTO> result = service.findAll(page);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
 	
+	}
+
 	@Test
 	public void deleteShouldDeleteResourceWhenIdExists() {
+
 		service.delete(existingId);
-		
+
 		Assertions.assertEquals(countTotalProducts - 1, repository.count());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
-		
+
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-			service.delete(nonExistingId);		
+			service.delete(nonExistingId);
 		});
 	}
 

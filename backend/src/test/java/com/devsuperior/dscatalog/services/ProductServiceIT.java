fbb1c696edb569a808de3a0c5b3f3384c18035f1
westorres9 <wester.torres9@gmail.com>
2022-04-14 00:19:24 -0300

    first Integration Tests in ProductService Layer with methods delete when Id exists and when Id Not exists

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,62 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+
+import com.devsuperior.dscatalog.DTO.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
+
+@SpringBootTest
+public class ProductServiceIT {
+	
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	private Long countTotalProducts;
+	private PageImpl<Product> page;
+	private Product product;
+	private ProductDTO productDTO;
+	private Category category;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+		product = Factory.createProduct();
+		productDTO = Factory.createProductDTO();
+		page = new PageImpl<>(List.of(product));
+		category = Factory.createCategory();
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);		
+		});
+	}
+
+}
